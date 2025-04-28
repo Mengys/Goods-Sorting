35142b4e5abcfa6f4711ddf7661e77bbf0235b0f
Ton3m <104393441+ton3m@users.noreply.github.com>
2025-04-28 19:38:55 +0300

    SoundPlayer created

--- Assets/_Project/Code/Architecture/Services/ConfigProvider/ConfigProvider.cs
+++ Assets/_Project/Code/Architecture/Services/ConfigProvider/ConfigProvider.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using _Project.Code.Architecture.Services.SoundPlayer;
 using UnityEngine;
 
 namespace _Project.Code
@@ -6,5 +9,16 @@ namespace _Project.Code
     public class ConfigProvider : ScriptableObject
     {
         [field: SerializeField] public GameStatesConfig GameStates { get; private set; }
+        [field: SerializeField] public SoundsConfig Sounds { get; private set; }
+
+        public SoundConfig GetConfigFor(SoundId id)
+        {
+            var config = Sounds.Sounds.FirstOrDefault(x => x.Id == id);
+
+            if (config.Id == SoundId.None)
+                throw new ArgumentException($"Sound with id {id} not found");
+            
+            return config;
+        }
     }
 }
---
+++ Assets/_Project/Code/Architecture/Services/SoundPlayer/Config/SoundId.cs
@@ -0,0 +1,8 @@
+namespace _Project.Code.Architecture.Services.SoundPlayer
+{
+    public enum SoundId
+    {
+        None = 0,
+        Click = 1,
+    }
+}
