9cf84b427c0fa7a8b52e0df66513b0bf4ef96830
Ton3m <104393441+ton3m@users.noreply.github.com>
2025-04-29 14:11:24 +0300

    Configs updated + Conflicts resolved

--- Assets/_Project/Code/Infrastructure/Entry/Bootstrappers/MenuBootstrapper.cs
+++ Assets/_Project/Code/Infrastructure/Entry/Bootstrappers/MenuBootstrapper.cs
@@ -1,23 +1,51 @@
-using _Project.Code.Infrastructure.GameStateMachine;
-using _Project.Code.Infrastructure.GameStateMachine.State;
+using _Project.Code.Services.Counter;
 using _Project.Code.Services.SceneArgs;
+using _Project.Code.Services.UIFactory;
+using _Project.Code.UI.UIRoot;
+using R3;
+using UnityEngine;
 using Zenject;
 
-namespace _Project.Code.Infrastructure.Entry.Bootstrappers
+namespace _Project.Code.Infrastructure.Bootrstrappers
 {
     public class MenuBootstrapper : MonoInstaller
     {
-        [Inject] private IStateMachine<GameStateId> _stateMachine;
         [Inject] private ISceneOutputArgs _args;
+        [Inject] private IUIRootUser _uiRootUser;
 
         private void Awake()
         {
-            _args.Output.Bind<string>().FromInstance("Hi from Menu!"); 
-            _stateMachine.Enter(GameStateId.Gameplay);            
+            InitializeRoot();
+            InitializeUI();
+            InitializeOutputArgs();
         }
-        
+
         public override void InstallBindings()
         {
+            Container.Bind<Counter>().To<Counter>().AsSingle();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Container.Resolve<Counter>().Increment();
+        }
+
+        private void OnDestroy() =>
+            _uiRootUser.Cleanup();
+
+        private void InitializeOutputArgs() =>
+            _args.Output.Bind<string>().FromInstance("Hi from Menu!");
+
+        private void InitializeRoot() =>
+            _uiRootUser.Initialize(Container.Resolve<IUIRoot>());
+
+        private void InitializeUI()
+        {
+            var uiRoot = Container.Resolve<UIRoot>();
+            var counter = Container.Resolve<Counter>();
+
+            uiRoot.Counters.ForEach(view => counter.Value.Subscribe(view.SetCounter));
         }
     }
 }
